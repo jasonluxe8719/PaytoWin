8f95239204d35b0bbf2c9d7f22574d89fea06cf5
Jason <jasonluxe8719@gmail.com>
2020-07-13 17:44:18 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+
+module.exports = {
+  entry: './dist/main.js',
+  output: {
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js']
+  }
+};
